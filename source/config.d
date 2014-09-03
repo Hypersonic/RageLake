f14f5e722b9e8349ee8d92f304599f699bac23e4
Josh Hofing <joshhofing@gmail.com>
2014-09-02 20:13:13 -0400

    Rewrite Config.getKeyType to use a much more space-efficient mapping

--- source/config.d
+++ source/config.d
@@ -1,22 +1,21 @@
+import std.stdio;
 import util : KeyState, KeyType;
 import deimos.ncurses.ncurses;
 
 class Config {
-    @safe static KeyType getKeyType(KeyState state) pure {
-        switch (state.keyCode) {
-        case KEY_LEFT:
-            return KeyType.MOVE_LEFT;
-        case KEY_RIGHT:
-            return KeyType.MOVE_RIGHT;
-        case KEY_UP:
-            return KeyType.MOVE_UP;
-        case KEY_DOWN:
-            return KeyType.MOVE_DOWN;
-        case 'q':
-        case 'Q':
-            return KeyType.QUIT;
-        default:
-            return KeyType.NONE;
-        }
+    static KeyType getKeyType(KeyState state) pure {
+        KeyType[int] keymap;
+        foreach (k; ['h', 'H', KEY_LEFT])
+            keymap[k] = KeyType.MOVE_LEFT;
+        foreach (k; ['l', 'L', KEY_RIGHT])
+            keymap[k] = KeyType.MOVE_RIGHT;
+        foreach (k; ['k', 'K', KEY_UP])
+            keymap[k] = KeyType.MOVE_UP;
+        foreach (k; ['j', 'J', KEY_DOWN])
+            keymap[k] = KeyType.MOVE_DOWN;
+        foreach (k; ['q', 'Q'])
+            keymap[k] = KeyType.QUIT;
+
+        return keymap.get(state.keyCode, KeyType.NONE);
     }
 }
