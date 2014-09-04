ce8eb005aafe0e95399efa4993d5159802e91442
Josh Hofing <joshhofing@gmail.com>
2014-09-04 12:38:17 -0400

    Add incorrect constructor fixing to Bounds, add unittests for Bounds.contains

--- source/util.d
+++ source/util.d
@@ -20,10 +20,46 @@ struct Point {
 
 struct Bounds {
     Point min, max;
-    @safe bool contains(Point p) pure {
-        return (min.x <= p.x && p.x < max.x
-             && min.y <= p.y && p.y < max.y);
+    this(Point min, Point max) {
+        // Correct min and max if they're not set up so that min is top left and max is bottom right
+        if (min.x > max.x) {
+            auto tmp = min.x;
+            min.x = max.x;
+            max.x = tmp;
+        }
+        if (min.y > max.y) {
+            auto tmp = min.y;
+            min.y = max.y;
+            max.y = tmp;
+        }
+        this.min = min;
+        this.max = max;
     }
+    @safe bool contains(Point p) pure {
+        return (min.x <= p.x && p.x <= max.x
+             && min.y <= p.y && p.y <= max.y);
+    }
+}
+
+unittest {
+    Point p = Point(10, 10);
+    
+    // Check that the point lies in a bounds it definetly should
+    Bounds b = Bounds(Point(0, 0), Point(20, 20));
+    assert(b.contains(p));
+
+    // Check that the right edge of a bounds that lies on a point counts as containing it
+    Bounds b2 = Bounds(Point(0, 0), Point(10, 10));
+    assert(b2.contains(p));
+
+    // Check that the left edge of a bounds that lies on a point counts as containing it
+    Bounds b3 = Bounds(Point(10, 10), Point(20, 20));
+    assert(b3.contains(p));
+    
+
+    // Check that a weird rectangle gets corrected in the constructor so contains still works
+    Bounds b4 = Bounds(Point(10, 0), Point(0, 10));
+    assert(b4.contains(p));
 }
 
 struct KeyState {
