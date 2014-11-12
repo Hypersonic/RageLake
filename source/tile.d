9de05a35cb81e39700ba76279a1ea8ae1ca8ce6e
Josh Hofing <joshhofing@gmail.com>
2014-11-11 22:53:15 -0500

    Remove TileType

--- source/tile.d
+++ source/tile.d
@@ -1,33 +1,18 @@
 import util;
 
-enum TileType {
-    NONE,
-    FLOOR,
-    WALL,
-}
-
 class Tile {
-    TileType type = TileType.NONE;
-
     @property Cell cell() {
         return Cell(' ', Color.UNIMPORTANT);
     }
 }
 
 class FloorTile : Tile {
-    @property this() {
-        this.type = TileType.FLOOR;
-    }
-
     override @property Cell cell() {
         return Cell('.', Color.UNIMPORTANT);
     }
 }
 
 class WallTile : Tile {
-    this() {
-        this.type = TileType.WALL;
-    }
 
     override @property Cell cell() {
         return Cell('#', Color.NORMAL);
