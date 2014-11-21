831881fd6131f9ed35adab5a76cb0ab159a8a9f1
Josh Hofing <joshhofing@gmail.com>
2014-11-21 18:17:10 -0500

    Add good rendering for the inventory screen

--- source/inventoryscreen.d
+++ source/inventoryscreen.d
@@ -1,7 +1,11 @@
+import std.string;
+import std.conv;
+import std.algorithm;
 import display;
 import screenstack;
 import inventory;
 import event;
+import logger;
 
 class InventoryScreen : Screen {
     Inventory inventory;
@@ -11,10 +15,12 @@ class InventoryScreen : Screen {
         isTransparent = false;
     }
 
+    // TODO: Add scrolling if we have more items than we can fit onscreen
     override void takeInput(KeyPress kp) {
         switch (kp.key) {
             case 127: // Backspace
             case 27: // ESC
+            case 'q':
                 import app;
                 screens.pop();
                 break;
@@ -26,8 +32,18 @@ class InventoryScreen : Screen {
     override void render(Display display) {
         int x = 10;
         int y = 10;
+        int maxitemwidth = 20;
+        // find the largest width. If it's less than maxitemwidth's value, use that instead
+        if (inventory.items.length > 0)
+            maxitemwidth = inventory.items.map!(item => item.name.length)().reduce!((a, b) => max(a, b))().to!int.max(maxitemwidth);
+        string top    = "Inventory".center(maxitemwidth + 4, '-');
+        string bottom = "".center(maxitemwidth + 4, '-');
+        string side = "|";
+        display.drawString(x, y++, top);
         foreach (item; inventory.items) {
-            display.drawString(x, y++, item.name);
+            if (y < display.height) // Only draw up to the height of the screen.
+                display.drawString(x, y++, side ~ " " ~ item.name.center(maxitemwidth) ~ " " ~ side);
         }
+        display.drawString(x, y++, bottom);
     }
 }
