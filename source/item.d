1470fae0a84b6739e426a760c1baae70a69a5442
Josh Hofing <joshhofing@gmail.com>
2014-11-22 13:37:01 -0500

    Add short and long descriptions to items

--- source/item.d
+++ source/item.d
@@ -1,3 +1,5 @@
 class Item {
     string name = "Unnamed Item";
+    string shortDescription = "Undescribed.";
+    string longDescription = "This item is undescribable, as the describer has not described it descriptively.";
 }
