5927798d760ee4b38f916f826b102f6b0812f8ac
Josh Hofing <joshhofing@gmail.com>
2014-11-23 18:42:50 -0500

    Add itemList and a simple way of inserting things into it

--- source/item.d
+++ source/item.d
@@ -3,3 +3,11 @@ class Item {
     string shortDescription = "Undescribed.";
     string longDescription = "This item is undescribable, as the describer has not described it descriptively.";
 }
+
+Item function()[] itemList = []; // This list will contain functions that return each kind of item
+
+// Simple template to add a static constructor that puts the class this is mixed into in the itemList
+// Requires that the class is a subclass of Item
+mixin template registerForItemList() {
+    static this() { itemList ~= function Item() { return new this(); }; }
+}
