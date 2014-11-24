e5e9ae4d8f5713d1245666eb92627b46c4b040fc
Josh Hofing <joshhofing@gmail.com>
2014-11-24 16:32:26 -0500

    Switch item registry addition to the slightly shorter anonymous function syntax

--- source/item.d
+++ source/item.d
@@ -10,7 +10,7 @@ Item function()[] itemList = []; // This list will contain functions that return
 // Requires that the class is a subclass of Item
 mixin template registerItem() {
     static if (is(this : Item)) {
-        static this() { itemList ~= function Item() { return new this(); }; }
+        static this() { itemList ~= () => new this(); }
     } else {
         alias T = this; // We need to alias here because using `this` on it's own causes a compilation error (possible compiler bug?)
         static assert(false, "Trying to register a class for the item registry which is not a subclass of Item: " ~ __traits(identifier, T));
