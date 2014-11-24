0dc181f30119ddc6bf967dc6c60049f7b6dadbd6
Josh Hofing <joshhofing@gmail.com>
2014-11-24 10:23:35 -0500

    Give a nice error message when you try to register a non-Item for the Item registry

--- source/item.d
+++ source/item.d
@@ -9,5 +9,10 @@ Item function()[] itemList = []; // This list will contain functions that return
 // Simple template to add a static constructor that puts the class this is mixed into in the itemList
 // Requires that the class is a subclass of Item
 mixin template registerItem() {
-    static this() { itemList ~= function Item() { return new this(); }; }
+    static if (is(this : Item)) {
+        static this() { itemList ~= function Item() { return new this(); }; }
+    } else {
+        alias T = this; // We need to alias here because using `this` on it's own causes a compilation error (possible compiler bug?)
+        static assert(false, "Trying to register a class for the item registry which is not a subclass of Item: " ~ __traits(identifier, T));
+    }
 }
