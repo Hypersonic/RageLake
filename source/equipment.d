8aa86aa374e2aeed72bfdd612c3b4e9090763545
Josh Hofing <joshhofing@gmail.com>
2014-11-25 14:25:16 -0500

    Subclass equipment to item

--- source/equipment.d
+++ source/equipment.d
@@ -1,7 +1,8 @@
 import entity;
+import item;
 
 // Something that can be equipped. Various methods will be called when the equipee experiences certain events
-class Equipment {
+class Equipment : Item {
     void onAttack(ref Entity equipee) {}
     void onHit(ref Entity equipee) {}
 }
