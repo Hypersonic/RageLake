81fe18580cf4ea8f27ea8ceb303804f9c32a90e2
Josh Hofing <joshhofing@gmail.com>
2014-11-25 19:32:36 -0500

    Make the test items Equipment instead of just items

--- source/items/testitems.d
+++ source/items/testitems.d
@@ -1,10 +1,20 @@
 import item;
+import equipment;
 
-class TestSword : Item {
+class TestSword : Equipment {
     mixin registerItem;
     this() {
         name = "Test Sword";
         shortDescription = "A Test Sword";
         longDescription = "This sword is a test. Go figure.";
+    }
+}
+
+class TestSpear: Equipment {
+    mixin registerItem;
+    this() {
+        name = "Test Spear";
+        shortDescription = "A Test Spear";
+        longDescription = "This spear is a test. Go figure.";
     }
 }
