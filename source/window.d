1a356f78acb31de39635463923b409cb1f2e4b04
Josh Hofing <joshhofing@gmail.com>
2014-12-17 23:28:38 -0500

    Add alignment options for windows' text

--- source/window.d
+++ source/window.d
@@ -4,16 +4,22 @@ import std.typecons;
 
 import display;
 
+enum Alignment {
+    ALIGN_LEFT,
+    ALIGN_CENTER,
+    ALIGN_RIGHT
+}
+
 class Window {
     string title;
-    Tuple!(string, Color)[] lines;
+    Tuple!(string, Color, Alignment)[] lines;
     
     this(string title = "") {
         this.title = title;
     }
 
-    void push(string rhs, Color color = Color.NORMAL) {
-        lines ~= tuple(rhs, color);
+    void push(string rhs, Color color = Color.NORMAL, Alignment alignment = Alignment.ALIGN_CENTER) {
+        lines ~= tuple(rhs, color, alignment);
     }
 
     int width() {
@@ -28,9 +34,23 @@ class Window {
         display.drawString(x, y, "+" ~ title.center(width + 2, '-') ~ "+");
         y++;
         foreach (line; lines) {
+            string alignedstring;
+            switch (line[2]) {
+                case Alignment.ALIGN_LEFT:
+                    alignedstring = line[0].leftJustify(width);
+                    break;
+                case Alignment.ALIGN_CENTER:
+                    alignedstring = line[0].center(width);
+                    break;
+                case Alignment.ALIGN_RIGHT:
+                    alignedstring = line[0].rightJustify(width);
+                    break;
+                default:
+                    break;
+            }
             display.drawString(x - 1, y, padding);
             display.drawString(x, y, "| ");
-            display.drawString(x+2, y, line[0].center(width), line[1]);
+            display.drawString(x+2, y, alignedstring, line[1]);
             display.drawString(x+2+width, y, " |");
             y++;
         }
