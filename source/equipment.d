afc8567a92da08ad76cbf783d2a481b9d24a014c
Josh Hofing <joshhofing@gmail.com>
2014-12-20 11:52:45 -0500

    Add EquipRegions

--- source/equipment.d
+++ source/equipment.d
@@ -2,10 +2,20 @@ import entity;
 import item;
 import action;
 
+enum EquipRegion {
+    HEAD,
+    TORSO,
+    LEFT_ARM,
+    RIGHT_ARM,
+    LEFT_LEG,
+    RIGHT_LEG,
+}
+
 // Something that can be equipped. Various methods will be called when the equipee experiences certain events
 class Equipment : Item {
     int maxDurability;
     int durability;
+    EquipRegion[] regions;
 
     void onAttack(ref Entity equipee, AttackAction attack) {}
     void onHit(ref Entity equipee, AttackAction attack) {}
