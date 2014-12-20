67562cb58430d94a5767fcf7d3e7e456e081b0e6
Josh Hofing <joshhofing@gmail.com>
2014-12-20 16:07:09 -0500

    Remove the "testing" part of the test weapons.

--- source/items/testitems.d
+++ source/items/testitems.d
@@ -7,14 +7,14 @@ import item;
 import equipment;
 import action;
 
-class TestSword : Equipment {
+class Sword : Equipment {
     mixin registerItem;
     this() {
-        name = "Test Sword";
-        shortDescription = "A Test Sword";
-        longDescription = "This sword is a test. Go figure.";
+        name = "Sword";
+        shortDescription = "A Sword";
+        longDescription = "This steel sword has looks sturdy and strong.";
         maxDurability = 10;
-        durability = 10;
+        durability = maxDurability;
         regions = [EquipRegion.RIGHT_ARM];
     }
 
@@ -27,19 +27,19 @@ class TestSword : Equipment {
     }
 }
 
-class TestSpear: Equipment {
+class Spear: Equipment {
     mixin registerItem;
     this() {
-        name = "Test Spear";
-        shortDescription = "A Test Spear";
-        longDescription = "This spear is a test. Go figure.";
-        maxDurability = 10;
-        durability = 10;
+        name = "Spear";
+        shortDescription = "A Spear";
+        longDescription = "A long, wooden spear with a point of solid steel.";
+        maxDurability = 11;
+        durability = maxDurability;
         regions = [EquipRegion.RIGHT_ARM, EquipRegion.LEFT_ARM];
     }
 
     override void onAttack(ref Entity equipee, AttackAction attack) {
-        double multiplier = 3;
+        double multiplier = 5;
         if (durability > 0) {
             attack.damage = cast(int) ceil(multiplier * attack.damage);
             durability--;
