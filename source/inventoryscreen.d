ea992c14e438aea4dda6593a41a063bd95d4ed00
Josh Hofing <joshhofing@gmail.com>
2014-12-23 08:48:58 -0500

    Use "Equipped" instead of just "e" for labelling equipped items

--- source/inventoryscreen.d
+++ source/inventoryscreen.d
@@ -121,7 +121,7 @@ class InventoryScreen : Screen {
                     
                 }
 
-                string suffix = list is inventory.items && inventory.equipment.canFind(item) ? " (e)" : "";
+                string suffix = list is inventory.items && inventory.equipment.canFind(item) ? " (equipped)" : "";
                 win.push(item.name ~ suffix, color);
             }
             win.render(display, x, y);
