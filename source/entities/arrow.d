5651b1355412a1956c9cc24ca81a2ad9c9449931
Josh Hofing <joshhofing@gmail.com>
2015-01-06 21:26:55 -0500

    Add Arrow class

---
+++ source/entities/arrow.d
@@ -0,0 +1,33 @@
+module entities.arrow;
+
+import entity;
+import util;
+import action;
+
+class Arrow : Entity {
+    int vx;
+    int vy;
+
+    this(World w, int vx, int vy) {
+        super(w);
+        normalColor = Color.ENEMY;
+        this.vx = vx;
+        this.vy = vy;
+        this.staminaRechargeRate = 10;
+        if (this.vx > 0) {
+            cell.glyph = '>';
+        } else if (this.vx < 0) {
+            cell.glyph = '<';
+        } else if (this.vy > 0) {
+            cell.glyph = '^';
+        } else {
+            cell.glyph = 'v';
+        }
+    }
+    
+    override Action update(World world) {
+        this.desiredAction = new MovementAction(this, vx, vy);
+        super.update(world);
+        return this.desiredAction;
+    }
+}
